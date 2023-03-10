d99c106a4359a2eab994f70307094320f643cbc4
basheerrjoub <basheer20599@gmail.com>
2023-03-10 21:09:43 +0200

    Addig a ViewSet class to the API routing

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
-urlpatterns = [path("hello/", views.HelloApiView.as_view())]
+router = DefaultRouter()
+router.register("view_set", views.ViewSet, base_name="view_set")
+
+urlpatterns = [
+    path("api_view/", views.HelloApiView.as_view()),
+    path("", include(router.urls)),
+]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api.serializers import HelloSerializer
 
 
@@ -37,3 +38,41 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Deleting an object in the database"""
         return Response({"method": "Delete"})
+
+
+class ViewSet(viewsets.ViewSet):
+    """Create the CRUD using ViewSet class"""
+
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        """Return a list of users"""
+        users = ["Ahmad", "Essa", "Mohammad"]
+
+        return Response({"message": "users", "names": users})
+
+    def create(self, request):
+        """Create a new user"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name} from ViewSet"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retrieving an object by the pk"""
+        return Response({"message": "Retrieve an object"})
+
+    def update(self, request, pk=None):
+        """Updating an object"""
+        return Response({"message": "Updating an object"})
+
+    def partial_update(self, request, pk=None):
+        """Updating a part of an object"""
+        return Response({"message": "Updating a part of an object"})
+
+    def destroy(self, request, pk=None):
+        """Deleting an object"""
+        return Response({"message": "Deleting an object"})
